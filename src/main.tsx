34d75fb348e0350e04e6569e3e2d7430e41b6bc2
Piyawat Wongyat <mask30254@gmail.com>
2025-02-12 21:17:19 +0700

    ✨feat[main.tsx]: wrap App component with EventsProvider for event context management

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './hooks/useAuth.tsx'
+import { EventsProvider } from './hooks/events-context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
+    <EventsProvider>
       <App />
+    </EventsProvider>
   </AuthProvider>
 )
